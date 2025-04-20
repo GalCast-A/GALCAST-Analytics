96c1d8f63401a630edccb166a1d7c4c374e0b1e6
GalCast-A <galnares@bc.edu>
2025-04-20 10:36:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -54,14 +54,9 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Configure CORS to allow requests from your website's domain
-CORS(app, resources={r"/analyze_portfolio": {"origins": [
-    "https://38b18bc9-59b7-441a-ad3c-18578f3d55bd.dev2.app-preview.com",
-    "https://sbi-fup3frty.bolt.new",
-    "https://bolt.new",
-    "http://localhost:5173",
-    "*.webcontainer-api.io"
-]}})
+# Configure CORS to allow requests from all origins (for testing)
+CORS(app, resources={r"/analyze_portfolio": {"origins": "*"}})
+
 class PortfolioAnalyzer:
     def __init__(self):
         self.today_date = datetime.now().strftime("%Y-%m-%d")
